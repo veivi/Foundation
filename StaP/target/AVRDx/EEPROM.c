0bfdb57f8bc18ab140023d8c2a457904752f489d
Juhani Vehvilainen <juhani.vehvilainen@fimnet.fi>
2024-09-20 20:44:36 +0300

    Update EEPROM.c

--- StaP/target/AVRDx/EEPROM.c
+++ StaP/target/AVRDx/EEPROM.c
@@ -4,10 +4,10 @@
 
 void EEPROM_Read(uint16_t offset, uint8_t *data, size_t size)
 {
-  eeprom_read_block(data, (void*) offset, size);
+  // eeprom_read_block(data, (void*) offset, size);
 }
 
 void EEPROM_Write(uint16_t offset, const uint8_t *data, size_t size)
 {
-  eeprom_write_block(data, (void*) offset, size);
+  // eeprom_write_block(data, (void*) offset, size);
 }
