fcbaf75098a72c0beeb866e61f78557c36a069b2
Juhani Vehvilainen <juhani.vehvilainen@fimnet.fi>
2024-10-07 19:22:14 +0300

    Update I2C.c

--- StaP/target/AVRDx/I2C.c
+++ StaP/target/AVRDx/I2C.c
@@ -82,14 +82,12 @@ static bool timedOut = false;
 void I2C_0_EndSession(void)
 {
     if(timedOut) {
-      TWI0.MCTRLB = TWI_FLUSH_bm;
+      // TWI0.MCTRLB = TWI_FLUSH_bm;
       TWI0.MSTATUS = TWI_BUSSTATE_IDLE_gc;
     } else
       TWI0.MCTRLB = TWI_MCMD_STOP_gc;
 
     timedOut = false;
-
-    STAP_DelayMillis(1);
 }
 
 static uint8_t i2c_0_WaitW(void)
