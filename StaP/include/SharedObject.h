107e042b97713400540f5fd30e894a7db3bad9d8
veivi <juhani.vehvilainen@fimnet.fi>
2024-10-08 13:24:03 +0300

    Update SharedObject.h

--- StaP/include/SharedObject.h
+++ StaP/include/SharedObject.h
@@ -10,4 +10,7 @@ struct SharedObject {
 #define SHARED_ACCESS_BEGIN(o)        sharedAccessBegin(&((o).header))
 #define SHARED_ACCESS_END(o)          sharedAccessEnd(&((o).header))
 
+void sharedAccessBegin(struct SharedObject *obj);
+void sharedAccessEnd(struct SharedObject *obj);
+
 #endif
