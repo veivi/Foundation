19e6b982083368abe7e5be166dfd0fb91c7a95e3
veivi <juhani.vehvilainen@fimnet.fi>
2024-10-08 12:06:38 +0300

    Create EventLog.h

---
+++ StaP/include/EventLog.h
@@ -0,0 +1,11 @@
+#ifndef STAP_EVENT_LOG_H
+#define STAP_EVENT_LOG_H
+
+enum StaP_ES {
+  StaP_ES_AlphaLink = 0,               // AlphaLink node ID
+  StaP_ES_Driver = 0x80                // Driver ID
+};
+
+typedef uint8_t StaP_EventSource_t;
+
+#endif
