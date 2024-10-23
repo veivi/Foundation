2eb497f7dad58d39cc220397602c6d02e268c328
Juhani Vehvilainen <juhani.vehvilainen@fimnet.fi>
2024-10-23 23:35:50 +0300

    Update Datagram.c

--- StaP/Datagram.c
+++ StaP/Datagram.c
@@ -374,7 +374,7 @@ void datagramRxInputWithHandler(DgLink_t *link, void (*handler)(void*, uint8_t n
   }
 }
 
-void datagramRxInput(DgLink_t *link, const uint8_t *data, size_t size), const uint8_t *buffer, size_t size)
+void datagramRxInput(DgLink_t *link, const uint8_t *data, size_t size)
 {
   datagramRxInputWithHandler(link, link->rxHandler, buffer. size);
 }
