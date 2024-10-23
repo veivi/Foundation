1d49f7c7c1a53bba42194e939ec23b40cf98abd2
veivi <juhani.vehvilainen@fimnet.fi>
2024-10-23 19:39:56 +0300

    Update ADC.c

--- StaP/target/AVRDx/ADC.c
+++ StaP/target/AVRDx/ADC.c
@@ -5,6 +5,26 @@
 #include "ADC.h"
 #include <avr/io.h>
 
+/* This function initializes the ADC module */
+void ADC0_init(void)
+{
+  /* Disable interrupt and digital input buffer on PD0 */
+  PORTD.PIN0CTRL &= ~PORT_ISC_gm;
+  PORTD.PIN0CTRL |= PORT_ISC_INPUT_DISABLE_gc;
+    
+  /* Disable pull-up resistor */
+  PORTD.PIN0CTRL &= ~PORT_PULLUPEN_bm;
+
+  VREF.ADC0REF = VREF_REFSEL_4V096_gc;  /* Internal 4.096V reference */
+
+    ADC0.CTRLC = ADC_PRESC_DIV4_gc;        /* CLK_PER divided by 4 */    
+    ADC0.CTRLA = ADC_ENABLE_bm             /* ADC Enable: enabled */
+               | ADC_RESSEL_12BIT_gc      /* 12-bit mode */
+               | ADC_LEFTADJ_bm;
+    ADC0.MUXPOS = ADC_MUXPOS_AIN0_gc;      /* Select ADC channel AIN0 <-> PD0 */
+}
+
+
 /* This function returns the ADC conversion result */
 uint16_t ADC0_read(void)
 {
