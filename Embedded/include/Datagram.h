a914a84410b489449a932a487ad876721cbdc46e
veivi <juhani.vehvilainen@fimnet.fi>
2025-05-05 14:10:28 +0300

    Update Datagram.h

--- Embedded/include/Datagram.h
+++ Embedded/include/Datagram.h
@@ -36,7 +36,9 @@ typedef struct DatagramLink {
   uint8_t rxSeqLast[DG_MAX_NODES];
   uint16_t datagramsGood[DG_MAX_NODES], datagramsLost[DG_MAX_NODES];
   uint8_t txSeq[DG_MAX_NODES];
-  uint16_t datagramBytes, datagramBytesRaw;
+  uint16_t totalRxBytes, totalRxBytesRaw;
+  uint16_t totalTxBytes, totalTxBytesRaw;
+  uint16_t totalRxDatagrams, totalTxDatagrams;
   VP_TIME_MILLIS_T datagramLastTxMillis, datagramLastRxMillis;
   uint8_t *rxStore;
   size_t rxStoreSize;
@@ -72,6 +74,7 @@ void datagramTxOut(DgLink_t*, const uint8_t *data, size_t l);
 void datagramTxEnd(DgLink_t*);
 void datagramRxInput(DgLink_t*, const uint8_t *data, size_t s);
 void datagramRxStatus(DgLink_t *link, uint8_t node, uint16_t *totalBuf, uint16_t *lostBuf);
+void datagramLinkStatus(DgLink_t *link, uint16_t *totalRxBytesBuf, uint16_t *totalTxBytesBuf, uint16_t *totalRxDgBuf, uint16_t *totalTxDgBuf);
 void datagramRxInputWithHandler(DgLink_t *link, void (*handler)(void*, uint8_t node, const uint8_t *data, size_t size), const uint8_t *buffer, size_t size);
 
 #endif
