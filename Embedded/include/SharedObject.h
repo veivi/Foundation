dc4c69d77e2fd8c4032680b83c1dbc562292edd3
Juhani Vehvilainen <juhani.vehvilainen@fimnet.fi>
2025-05-26 20:58:15 +0300

    Update SharedObject.h

--- Embedded/include/SharedObject.h
+++ Embedded/include/SharedObject.h
@@ -11,6 +11,12 @@ struct SharedObject {
 #define SHARED_ACCESS_BEGIN(o)        sharedAccessBegin(&((o).header))
 #define SHARED_ACCESS_END(o)          sharedAccessEnd(&((o).header))
 
+#define SHARED_OBJECT_READ(r, f, v) \
+  (SHARED_ACCESS_BEGIN(r), v = (r).f, SHARED_ACCESS_END(r), v) 
+#define SHARED_OBJECT_UPDATE(r, f, v) \
+  (SHARED_ACCESS_BEGIN(r), (r).f = v, SHARED_ACCESS_END(r)) 
+
+
 void sharedAccessBegin(struct SharedObject *obj);
 void sharedAccessEnd(struct SharedObject *obj);
 
